3d81fdb7f52535b53575d01de627f8b3033c6635
saurabh4mar <saurabh4393@gnail.com>
2014-02-20 04:04:06 +0530

    first

---
+++ projes/application/views/footer.php
@@ -0,0 +1,18 @@
+
+<div class="row">
+    <div class="span12">
+        
+        <div class="well">
+            <center><h1> <strong>list grid view </strong>    </h1></center>
+        </div>
+    
+    </div>    
+   </div>
+
+
+
+
+
+</div>
+</body>
+</html>
---
+++ projes/application/views/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<head>
+  </head>
+<body>
+    
+    <link rel="stylesheet" type= "text/css" href="<?php echo base_url()."css/bootstrap.css"           ?>"/>
+    
+   
+    
+          <div class="container">
+              <div class="row">
+                  <div class="span12">
+                      <div class="well">
+                           <center><h1> header section </h1></center>
+                           
+                           
+                           
+                  </div>
+                  </div> 
+                  </div>
+      
+    
